15aab14b6909292840fc19c9e49266ea39a5bb8a
ifty_chowdhuryy <nblues399@gmail.com>
2021-10-05 19:23:00 +0600

    the0therguy

--- app.js
+++ app.js
@@ -26,4 +26,10 @@ function addTodo(event){
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class ="fas fa-check"></i>';
     completedButton.classList.add("complete-btn");
+
+    //CHECK trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
 }
